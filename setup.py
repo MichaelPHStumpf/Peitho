7c736a08b2b6df1ae731fdae35d3ae86525d6a02
jonasmac16 <jonas.mackerodt16@imperial.ac.uk>
2017-08-07 09:33:17 +0100

    updated setup files

--- setup.py
+++ setup.py
@@ -3,9 +3,9 @@ from setuptools import setup
 setup(name='peitho',
 	version='0.1',
 	description='Perfecting Experiments with Information Theory',
-	url='https://github.com/ld2113/Experimental-Design',
+	url='https://github.com/MichaelPHStumpf/Peitho',
 	author='Leander Dony, Scott Ward, Jonas Mackerodt',
-	author_email='jonas.mackerodt16@imperial.ac.uk',
+	author_email='m.stumpf@imperial.ac.uk',
 	license='MIT',
 	packages=['peitho'],
 	install_requires = [
@@ -17,4 +17,11 @@ setup(name='peitho',
 	entry_points = {
 		'console_scripts': ['peitho=peitho.main.main:main']
 		},
+	keywords = ['information theory','entropy','experimental design'],
+	classifiers = ['Development Status :: 3 - Alpha',
+	'Environment :: Console',
+	'Intended Audience :: Science/Research',
+	'Topic :: Scientific/Engineering :: Bio-Informatics',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 2.7'],
 	zip_safe=False)
